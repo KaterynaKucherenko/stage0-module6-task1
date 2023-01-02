5a9f5d6409d9f68247f74875f9189da4e4f59cff
KaterynaKucherenko <estorskaya@gmail.com>
2023-01-02 20:21:38 +0300

    done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,21 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
-}
+            private String color;
+	    private int numberOfPaws;
+	    private boolean hasFur;
+	    
+	    public Animal (String color, int numberOfPaws, boolean hasFur){
+	        this.color = color;
+	        this.numberOfPaws = numberOfPaws;
+	        this.hasFur = hasFur;}
+
+	public  String getDescription(){
+	    String paw = " paw ";
+	    String hasFurPrint = "no ";
+	    if (numberOfPaws>1){
+	        paw = " paws ";}
+	    if (hasFur == true){
+	        hasFurPrint= "a ";}
+	   return "This animal is mostly " + color + ". It has " + numberOfPaws +  paw +"and " + hasFurPrint +  "fur.";}}
+
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
-}
+ public Bird (String color, int numberOfPaws, boolean hasFur){
+            super ( color, numberOfPaws, hasFur);}
+            
+        public String getDescription(){
+	    String paw = " paw ";
+	    String hasFurPrint = "no ";
+	    if (super.numberOfPaws>1){
+	        paw = " paws ";}
+	    if (super.hasFur == true){
+	        hasFurPrint= "a ";}
+	       
+	        
+	   return "This animal is mostly " + super.color + ". It has " + super.numberOfPaws +  paw +"and " + hasFurPrint +  "fur. Moreover, it has 2 wings and can fly.";
+        
+    }}
+
