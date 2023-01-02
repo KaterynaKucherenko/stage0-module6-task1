8a3c3327ae5eeb88d3ff1d7820b31db17ad4b121
KaterynaKucherenko <estorskaya@gmail.com>
2023-01-02 20:57:49 +0300

    fix1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,22 +1,46 @@
 package com.epam.mjc.stage0;
 
+
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
-            private String color;
-	    private int numberOfPaws;
-	    private boolean hasFur;
-	    
-	    public Animal (String color, int numberOfPaws, boolean hasFur){
-	        this.color = color;
-	        this.numberOfPaws = numberOfPaws;
-	        this.hasFur = hasFur;}
+    public Animal (String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;}
 
-	public  String getDescription(){
-	    String paw = " paw ";
-	    String hasFurPrint = "no ";
-	    if (numberOfPaws>1){
-	        paw = " paws ";}
-	    if (hasFur == true){
-	        hasFurPrint= "a ";}
-	   return "This animal is mostly " + color + ". It has " + numberOfPaws +  paw +"and " + hasFurPrint +  "fur.";}}
+    public String getColor(){
+        return this.color;}
+    public int getNumberOfPaws(){
+        return this.numberOfPaws;}
+
+    public  String getDescription(){
+        String paw = " paw ";
+        String hasFurPrint = "no ";
+        if (numberOfPaws>1){
+            paw = " paws ";}
+        if (hasFur == true){
+            hasFurPrint= "a ";}
+        return "This animal is mostly " + color + ". It has " + numberOfPaws +  paw +"and " + hasFurPrint +  "fur.";}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -2,19 +2,17 @@ package com.epam.mjc.stage0;
 
 public class Bird extends Animal {
 
- public Bird (String color, int numberOfPaws, boolean hasFur){
-            super ( color, numberOfPaws, hasFur);}
-            
-        public String getDescription(){
-	    String paw = " paw ";
-	    String hasFurPrint = "no ";
-	    if (super.numberOfPaws>1){
-	        paw = " paws ";}
-	    if (super.hasFur == true){
-	        hasFurPrint= "a ";}
-	       
-	        
-	   return "This animal is mostly " + super.color + ". It has " + super.numberOfPaws +  paw +"and " + hasFurPrint +  "fur. Moreover, it has 2 wings and can fly.";
-        
-    }}
+    public Bird (String color, int numberOfPaws, boolean hasFur){
+        super ( color, numberOfPaws, hasFur);}
 
+    public String getDescription(){
+        String birdColor = getColor();
+        int birdNumberOfPaws = getNumberOfPaws();
+        String paw = " paw ";
+        String hasFurPrint = "no ";
+       if (birdNumberOfPaws>1){
+          paw = " paws ";}
+
+        return "This animal is mostly " + birdColor + ". It has " +birdNumberOfPaws +  paw + "and no fur. Moreover, it has 2 wings and can fly.";
+
+    }}
